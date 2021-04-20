7782bd6f27f7ee2cb1d47f7a90c82529ef0f4439
BaoHungNguyen <baohung2412@gmail.com>
2021-04-20 17:51:21 +0700

    Update RNVideoTrimmer.swift

--- ios/RNVideoProcessing/RNVideoTrimmer/RNVideoTrimmer.swift
+++ ios/RNVideoProcessing/RNVideoTrimmer/RNVideoTrimmer.swift
@@ -455,7 +455,6 @@ class RNVideoTrimmer: NSObject {
       let minimumBitrate = options.object(forKey: "minimumBitrate") as? Float
       let saveWithCurrentDate = options.object(forKey: "saveWithCurrentDate") as? Bool ?? false
       let removeAudio = options.object(forKey: "removeAudio") as? Bool ?? false
-      let isHighEffect = options.object(forKey: "isHighEffect") as? Bool ?? false
 
       let manager = FileManager.default
       guard let documentDirectory = try? manager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
@@ -514,28 +513,15 @@ class RNVideoTrimmer: NSObject {
         metaItem.value = NSDate()
         compressionEncoder!.metadata = [metaItem]
       }
-      if #available(iOS 11.0, *) {
-        compressionEncoder?.videoSettings = [
-            AVVideoCodecKey: isHighEffect ? AVVideoCodecHEVC : AVVideoCodecH264,
-            AVVideoWidthKey: NSNumber.init(value: width!),
-            AVVideoHeightKey: NSNumber.init(value: height!),
-            AVVideoCompressionPropertiesKey: [
-                AVVideoAverageBitRateKey: NSNumber.init(value: averageBitrate),
-                AVVideoProfileLevelKey: AVVideoProfileLevelH264HighAutoLevel
-            ]
+    compressionEncoder?.videoSettings = [
+        AVVideoCodecKey: AVVideoCodecH264,
+        AVVideoWidthKey: NSNumber.init(value: width!),
+        AVVideoHeightKey: NSNumber.init(value: height!),
+        AVVideoCompressionPropertiesKey: [
+            AVVideoAverageBitRateKey: NSNumber.init(value: averageBitrate),
+            AVVideoProfileLevelKey: AVVideoProfileLevelH264HighAutoLevel
         ]
-      } else {
-        // Fallback on earlier versions
-        compressionEncoder?.videoSettings = [
-            AVVideoCodecKey: AVVideoCodecH264,
-            AVVideoWidthKey: NSNumber.init(value: width!),
-            AVVideoHeightKey: NSNumber.init(value: height!),
-            AVVideoCompressionPropertiesKey: [
-                AVVideoAverageBitRateKey: NSNumber.init(value: averageBitrate),
-                AVVideoProfileLevelKey: AVVideoProfileLevelH264HighAutoLevel
-            ]
-        ]
-      }
+    ]
       if !removeAudio {
         compressionEncoder?.audioSettings = [
             AVFormatIDKey: kAudioFormatMPEG4AAC,
